61087a31c38190b2005ec59b8ba1d606c1320633
Fabio Soutto Dev <fgimenes905@gmail.com>
2023-08-07 13:33:08 -0300

    add logo page index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,6 +10,9 @@ export default function Home() {
     <section className="flex flex-wrap items-center justify-center w-[100%] h-[100vh] p-[30px]">
 
       <div className="flex flex-col items-center justify-center w-[100%]">
+        <Link href="https://evolluo.com/" target="_blank">
+          <img src="/images/v2.png" width={200} height={200}/>
+        </Link>
         <p className="text-[25px] md:text-[40px] mb-[30px]">Seja bem-vindo(a) a <span className="font-extrabold text-[#2188E8]">Eva!</span></p>
         <h2 className="text-[30px] md:text-[60px]">Resolva tarefas do <span className="font-extrabold text-[#2188E8]">RH e T&D</span></h2>
         <h2 className="text-[30px] md:text-[60px] mb-[50px]">e crie conteúdos em segundos.</h2>
